631de602bd126e94b8956bf7f4a505b3f70ee789
vinsgo <vinsgo.tp@gmail.com>
2016-03-03 12:56:07 -0500

    little correction : name of the function and comments

--- VinsgoCoin.sol
+++ VinsgoCoin.sol
@@ -47,21 +47,22 @@ contract VinsgoCoin {
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
-    function MyToken(uint256 _supply, string _name, string _symbol, uint8 _decimals) {
+    function VinsgoCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) {
         /* if supply not given then generate arround 1 million of the smallest unit of the token */
         if (_supply == 0) _supply = 1020304;
 
         /* Unless you add other functions these variables will never change */
         balanceOf[msg.sender] = _supply;
-        name = _name;
-        symbol = _symbol;
+        name = _name; // still possible to chnage the name at the creation of the contract
+        symbol = _symbol; // idem for symbol
 
         /* If you want a divisible token then add the amount of decimals the base unit has  */
         decimals = _decimals;
-    }
+    } &
+
 
     /* Send coins */
-    function transfer(address _to, uint256 _value) {
+    function send(address _to, uint256 _value) {
         /* if the sender doenst have enough balance then stop */
         if (balanceOf[msg.sender] < _value) throw;
         if (balanceOf[_to] + _value < balanceOf[_to]) throw;
