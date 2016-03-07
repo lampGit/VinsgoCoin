ee3924dfd7f290ce5182a1a44c1606cd00d0f682
vinsgo <vinsgo.tp@gmail.com>
2016-03-07 18:53:10 -0500

    Update VinsgoCoin.sol

--- VinsgoCoin.sol
+++ VinsgoCoin.sol
@@ -44,12 +44,12 @@ contract VinsgoCoin {
     mapping (address => uint256) public balanceOf;
 
     /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    event CoinSend(address indexed from, address indexed to, uint256 value);
 
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function VinsgoCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) {
         /* if supply not given then generate arround 1 million of the smallest unit of the token */
-        if (_supply == 0) _supply = 1020304;
+        //if (_supply == 0) _supply = 1020304;
 
         /* Unless you add other functions these variables will never change */
         balanceOf[msg.sender] = _supply;
@@ -72,6 +72,6 @@ contract VinsgoCoin {
         balanceOf[_to] += _value;
 
         /* Notifiy anyone listening that this transfer took place */
-        Transfer(msg.sender, _to, _value);
+        CoinSend(msg.sender, _to, _value);
     }
 }
